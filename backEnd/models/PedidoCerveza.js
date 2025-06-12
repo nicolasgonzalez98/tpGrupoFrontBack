be0df89c11a65bc65ec5edd9952f29867aef9052
Nicolas Nahuel Gonzalez <nicolasgonzalez470@gmail.com>
2025-06-12 19:04:29 -0300

    auth controller

--- backEnd/models/PedidoCerveza.js
+++ backEnd/models/PedidoCerveza.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 
+//En duda
 const pedidoCervezaSchema = new mongoose.Schema({
   pedido_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Pedido', required: true },
   cerveza_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Cerveza', required: true },
